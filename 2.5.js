ca9aef9bee42e556b9e93dbb0038c8d32dda5c8c
Tess <tess@DESKTOP-9F1581K>
2023-12-07 20:51:56 +0100

    c'est mieux quand on ajoute les fichiers

---
+++ 2.5.js
@@ -0,0 +1,8 @@
+let favNumber = Number(prompt("What's your favorite number ?"));
+
+if (favNumber !== 42){
+    alert("Are you sure ?");
+    let favNumber = Number(prompt("What's your favorite number ?"));
+} else {
+    alert("42 it's the sens of the life !");
+}
---
+++ 2.6.js
@@ -0,0 +1,3 @@
+let nbr = prompt("Enter a number between 1 ans 7");
+
+switch
