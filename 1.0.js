b02c6ef29d277d3668eeedb07a16b3342f699936
Tess <tess@DESKTOP-9F1581K>
2023-12-07 14:56:57 +0100

    1.0; 1.1; 1.2; 2.1

---
+++ 1.0.js
@@ -0,0 +1,7 @@
+let nbr1 = 10;
+let nbr2 = 33;
+console.log(nbr2 + nbr2);
+console.log(nbr2 - nbr2);
+console.log(nbr2 * nbr2);
+console.log(nbr2 / nbr2);
+console.log(nbr2 % nbr2);
---
+++ 2.1.js
@@ -0,0 +1,9 @@
+let age = prompt("what's your age ?");
+
+if (age < 18 ){
+    alert("You are not yet an adult");
+}
+else {
+    alert("You are an adult");
+};
+
