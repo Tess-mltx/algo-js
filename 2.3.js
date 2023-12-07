a44f1edf8292e868d3d4ece47f6859f952a02c30
Tess <tess@DESKTOP-9F1581K>
2023-12-07 16:21:01 +0100

    2.3

---
+++ 2.3.js
@@ -0,0 +1,13 @@
+for (let i = 1; i <= 50; i += 1) {
+    if ((i % 2) === 0 ) {
+        console.log(i);
+    }
+}
+
+let j = 1;
+while (j <= 100) {
+    if ((j % 2) === 0 ) {
+        console.log(j);
+    }
+    j += 1;
+}
