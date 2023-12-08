b418baa46d3da96af3093c422e0d4320d0a65899
Tess <tess@DESKTOP-9F1581K>
2023-12-08 09:56:18 +0100

    to 2.7

--- 2.6.js
+++ 2.6.js
@@ -1,3 +1,27 @@
 let nbr = prompt("Enter a number between 1 ans 7");
 
-switch
+switch(nbr) {
+    case '1' :
+        alert(Monday);
+        break;
+    case '2' :
+        alert(tuesday);
+        break;
+    case '3' :
+        alert(Wednesday);
+        break;
+    case '4' :
+        alert(Thursday);
+        break;
+    case '5' :
+        alert(Friday);
+        break;
+    case '6' :
+        alert(Saturday);
+        break;
+    case '7' :
+        alert(Sunday);
+        break;
+    default :
+        alert("Sory it does'nt exist");
+}
---
+++ 2.7.js
@@ -0,0 +1,11 @@
+let n = parseInt(prompt("Choose a small number"));
+let answer = 0;
+y = 0 ;
+
+
+for (i = 1; i <= n; i += 1){
+    let answer = parseInt(prompt("An a other !"));
+    y = y + answer;
+}
+
+alert("The somme is " + y);
